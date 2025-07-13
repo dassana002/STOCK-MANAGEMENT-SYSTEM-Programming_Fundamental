d2e42311df602125ddf85d87f33d947d15c84564
dassana <dassana002@gmail.com>
2025-07-13 13:34:07 +0530

    change the credentials page is  Completed

--- Coursework.java
+++ Coursework.java
@@ -1,8 +1,8 @@
 import java.util.Scanner;
 
 class Coursework {
-    static final String user_Name = "Dassana";
-    static final String user_Password = "Dassana@123";
+    static String user_Name = "Dassana";
+    static String user_Password = "Dassana@123";
 
     private static final void clearConsole() {
         final String os = System.getProperty("os.name");
@@ -20,30 +20,75 @@ class Coursework {
         }
     }
 
-    private static void exit_the_System() {
+    public static void exit_the_System() {
         
     }
 
-    private static void log_out() {
+    public static void supplier_Manage() {
         
     }
 
-    private static void supplier_Manage() {
-        
+    public static void change_The_Credentials() {
+        Scanner input = new Scanner(System.in);
+
+        clearConsole();
+
+        System.out.println("+---------------------------------------------------------------------------+");
+        System.out.println("|\t\t\t\tCREDENTIAL MANAGE\t\t\t\t|");
+        System.out.println("+---------------------------------------------------------------------------+");
+
+        L1: while (true) {
+            System.out.print("Please enter the user name to verfy it's you: ");
+            String name = input.nextLine();
+
+            if (user_Name.equals(name)) {
+                System.out.println("Hey " + user_Name+"\n");
+
+                L2: while (true) {
+                    System.out.print("Enter your current password: ");
+                    String password = input.nextLine();
+
+                    if (user_Password.equals(password)) {
+                        System.out.print("Enter your new password: ");
+                        String new_password = input.nextLine();
+
+                        user_Password = new_password;
+
+                        L3:while (true) {
+                            System.out.print("password changed succesfully. Do you want to go home page (Y/N): ");
+                            char option = input.next().charAt(0);
+
+                            switch (option) {
+                                case 'y', 'Y' -> home();
+                                case 'n', 'N' -> login();
+                                default -> {
+                                    System.out.println("You entered the wrong option. Please correct the option again!\n");
+                                    continue L3;
+                                }
+                            }
+                        }
+                    } else {
+                        System.out.println("incorrect password. try again!\n");
+                        continue L2;
+                    }
+                }
+            } else {
+                System.out.println("Invalid User name. Please try again!\n");
+                continue L1;
+            }
+        }
+
     }
 
-    private static void change_The_Credentials() {
-        
-    }
-
-    public static void Menu() {
+    public static void home() {
         Scanner input = new Scanner(System.in);
 
         clearConsole();
 
         System.out.println("+---------------------------------------------------------------------------------------+");
         System.out.println("|\t\t\t\tWELCOME TO STOCK MANAGEMENT SYSTEM\t\t\t\t|");
-        System.out.println("+---------------------------------------------------------------------------------------+\n");
+        System.out
+                .println("+---------------------------------------------------------------------------------------+\n");
 
         System.out.println("[1] Change the Credentials\t [2] Suppliar Manage");
         System.out.println("[3] Stock  Manage\t\t [4] Log out");
@@ -55,19 +100,20 @@ class Coursework {
             char option = input.next().charAt(0);
 
             switch (option) {
-                case '1'-> change_The_Credentials();
-                case '2'-> supplier_Manage();
-                case '3'-> supplier_Manage();
-                case '4'-> log_out();
-                case '5'-> exit_the_System();
-                default-> {
+                case '1' -> change_The_Credentials();
+                case '2' -> supplier_Manage();
+                case '3' -> supplier_Manage();
+                case '4' -> login();
+                case '5' -> exit_the_System();
+                default -> {
+                    System.out.println("You entered the wrong option. Please correct the option again!\n");
                     continue;
                 }
             }
         }
     }
 
-    public static void homePage() {
+    public static void login() {
         Scanner input = new Scanner(System.in);
 
         clearConsole();
@@ -89,22 +135,23 @@ class Coursework {
 
                     if (user_Password.equals(password)) {
                         System.out.println("Login successful!");
-                        Menu();
-                        break L1;
+                        home();
                     } else {
-                        System.out.println("password is incorrect. please try again!");
+                        System.out.println("password is incorrect. please try again!\n");
                         continue L2;
                     }
                 }
             } else {
-                System.out.println("user name is invalid. please try again!");
+                System.out.println("user name is invalid. please try again!\n");
                 continue L1;
             }
         }
     }
 
     public static void main(String[] args) {
-        // homePage();
-        Menu();
+        // login();
+        // home();
+        // change_The_Credentials();
+        exit_the_System();
     }
 }
